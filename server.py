d29a4c19206a311df09409ba49b59d93a7d51ede
Tsung Lin Yong <tsunglin@ualberta.ca>
2014-01-18 16:36:37 -0700

    Implement basic HTTP 1.1 webserver

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import SocketServer
 # coding: utf-8
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Tsung Lin Yong
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,10 +30,75 @@ import SocketServer
 
 class MyWebServer(SocketServer.BaseRequestHandler):
     
+    STATUS_OK = "HTTP/1.1 200 OK"
+    STATUS_NOT_FOUND = "HTTP/1.1 404 Not Found"
+
     def handle(self):
+
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+
+        httpGetLine = self.data.splitlines()[0]
+
+        response = ""
+
+        requestedFilePath = self.getRequestedFilePath(httpGetLine)
+        contentType = self.getContentType(requestedFilePath)
+
+        # make sure the path exists and is located in ./www/
+        if (self.isPathInDirectory(requestedFilePath,
+                                   os.path.join(os.getcwd(), "www")) and
+            os.path.exists(requestedFilePath)):
+
+            with open(requestedFilePath) as f:
+
+                response = ("%s\r\n"
+                            "Connection: close\r\n"
+                            "Content-Type: %s\r\n"
+                            "Content-Length: %s\r\n\r\n"
+                            "%s" %
+                            (self.STATUS_OK, contentType,
+                             os.path.getsize(requestedFilePath), f.read()))
+
+        # the path doesn't exist, so return 404 not found
+        else:
+
+            response = ("%s\r\n"
+                        "Connection: close\r\n"
+                        "Content-Type: %s\r\n"
+                        "Content-Length: 0" %
+                        (self.STATUS_NOT_FOUND, contentType))
+                    
+        self.request.sendall(response)
+
+    def getRequestedFilePath(self, httpGetLine):
+
+        requestedFile = httpGetLine.split()[1]
+
+        requestedFilePath = os.path.join(os.getcwd(),
+                                         "www",
+                                         requestedFile[1:])
+
+        # if the requested path is a directory, serve the index file
+        if os.path.isdir(requestedFilePath):
+            requestedFilePath = os.path.join(requestedFilePath,
+                                                     "index.html")
+
+        return requestedFilePath
+
+    def getContentType(self, requestedFile):
+
+        fileName, fileExtension = os.path.splitext(requestedFile)
+        return "text/%s" % fileExtension[1:]
+
+    def isPathInDirectory(self, path, directory):
+
+        directoryAbs = os.path.abspath(directory)
+        pathAbs = os.path.abspath(path)
+
+        if pathAbs[:len(directoryAbs)] == directoryAbs:
+            return True
+
+        return False
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
